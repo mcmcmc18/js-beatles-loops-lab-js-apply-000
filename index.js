e4aed382c3eadb47f5e22ca8bf36e8b2849d4587
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-21 17:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for(var i = 0; i < musicians.length && i < instruments.length; i++){
+    var newString = `${musicians[i]} plays ${instruments[i]}`;
+    newArray.push(newString);
+  }
+  return newArray;
+}
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length){
+    var excited = '!!!';
+    facts[i] = facts[i] + excited;
+    i++;
+   }
+   return facts; 
+}
+
+function iLoveTheBeatles(num){
+var loveArray = [];
+var newNum = num + 1;
+do {
+  var saying = "I love the Beatles!";
+  loveArray.push(saying);
+}
+  while (loveArray.length < newNum);
+  return loveArray;
+}
